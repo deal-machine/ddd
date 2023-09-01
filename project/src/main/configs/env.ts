414a8e5f99625bee6c005900f9e17814a7f6033d
deal-machine <95deal@gmail.com>
2023-08-31 22:35:25 -0300

    up docker

---
+++ project/src/main/configs/env.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+
+export const env = {
+  isTest: process.env.NODE_ENV == "test",
+
+  dbHost: String(process.env.POSTGRES_HOST),
+  dbPort: Number(process.env.POSTGRES_PORT),
+  dbName: String(process.env.POSTGRES_DATABASE),
+  dbUser: String(process.env.POSTGRES_USERNAME),
+  dbPassword: String(process.env.POSTGRES_PASSWORD),
+  dbDialect: String(process.env.POSTGRES_DIALECT),
+
+  dbTestHost: String(process.env.POSTGRES_HOST_TEST),
+  dbTestPort: Number(process.env.POSTGRES_PORT_TEST),
+  dbTestName: String(process.env.POSTGRES_DATABASE_TEST),
+  dbTestUser: String(process.env.POSTGRES_USERNAME_TEST),
+  dbTestPassword: String(process.env.POSTGRES_PASSWORD_TEST),
+  dbTestDialect: String(process.env.POSTGRES_DIALECT_TEST),
+};
